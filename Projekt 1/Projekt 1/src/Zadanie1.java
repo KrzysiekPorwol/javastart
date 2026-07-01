7aba5679259cf6d64523615a91d8ed918e517969
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-07-01 15:21:56 +0200

    Dzien 1 kursu

---
+++ Projekt 1/Projekt 1/src/Zadanie1.java
@@ -0,0 +1,14 @@
+public class Zadanie1 {
+  static void main(String[] args) {
+    calydialog();
+  }
+
+  static void calydialog() {
+    System.out.println("Cześć Jan, słyszałem, że masz " + 25 + "lat i uczysz się programowania. Czy to prawda?");
+    System.out.println(true);
+    System.out.println("Czy Twoje inicjały to " + 'J' +  'K' + "?");
+    System.out.println("Tak, " + true);
+  }
+}
+
+
