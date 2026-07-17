61a37c77cd11b3cf5d3c25a2bc622fdcb7eddbfc
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-07-17 08:44:26 +0200

    Library v0.4

--- Project 2 - libary/Project 2 - libary/src/Book.java
+++ Project 2 - libary/Project 2 - libary/src/Book.java
@@ -6,20 +6,25 @@ public class Book {
   String publisher;
   String isbn;
 
-  Book(String newTitle, String newAuthor, int newReleaseDate,
-       int newPages, String newPublisher, String newIsbn) {
 
-    title = newTitle;
-    author = newAuthor;
-    releaseDate = newReleaseDate;
-    pages = newPages;
-    publisher = newPublisher;
-    isbn = newIsbn;
+  Book(String title, String author, int releaseDate, int pages,
+       String publisher, String isbn) {
+    this(title, author, releaseDate, pages, publisher);
+    this.isbn = isbn;
+  }
+
+  Book(String title, String author, int releaseDate,
+       int pages, String publisher) {
+    this.title = title;
+    this.author = author;
+    this.releaseDate = releaseDate;
+    this.pages = pages;
+    this.publisher = publisher;
   }
 
   void printInfo() {
-    String printInfo = title + " | " + author + " | " + releaseDate  + " | "
-          + pages  + " | " + publisher  + " | " + isbn;
+    String printInfo = title + " | " + author + " | " + releaseDate + " | "
+          + pages + " | " + publisher + " | " + isbn;
     System.out.println(printInfo);
   }
 }
