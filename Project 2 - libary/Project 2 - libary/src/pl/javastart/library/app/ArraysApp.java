92835eb718445a9dc0142a4be252b625d93739bc
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-07-21 11:11:05 +0200

    Tablice jednowymiarowe - ćwiczenie 1 Niestety zle zrozumialem zadanie.

---
+++ Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/ArraysApp.java
@@ -0,0 +1,23 @@
+package pl.javastart.library.app;
+
+import pl.javastart.library.model.Arrays;
+
+public class ArraysApp {
+ public static void main() {
+    Arrays arrays = new Arrays();
+
+   System.out.println(arrays.array1[0]+arrays.array1[1]+arrays.array1[2]+arrays.array2[0]+arrays.array2[1]+arrays.array2[2]);
+
+   Arrays arrays1 = new Arrays();
+
+   System.out.println(arrays.array1.length + arrays.array2.length);
+  }
+}
+
+
+//      Napisz program, w którym zadeklarujesz dwie tablice typu całkowitoliczbowego.
+//      Każda z nich powinna przechowywać po 3 dowolne liczby typu int.
+//      Program powinien wyświetlić sumę wszystkich liczb z obu tablic.
+//
+//Następnie zdefiniuj metodę, która realizuje to samo zadanie, czyli przyjmuje dwie tablice,
+// a zwraca sumę wszystkich ich elementów.
