40af814b545fdc61a43b1e8e4ff1ad2174b56512
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-08-14 07:32:39 +0200

    Library v1.8.2

--- Project 2 - libary/Project 2 - libary/src/pl/javastart/library/model/Magazine.java
+++ Project 2 - libary/Project 2 - libary/src/pl/javastart/library/model/Magazine.java
@@ -1,6 +1,8 @@
 package pl.javastart.library.model;
 
 public class Magazine extends Publication {
+    public static final String TYPE = "Magazyn";
+
     private int month;
     private int day;
     private String language;
@@ -29,6 +31,17 @@ public class Magazine extends Publication {
     }
     public void setLanguage(String language) {
         this.language = language;
+    }
+
+    @Override
+    public String toCsv() {
+        return (TYPE + ";") +
+              getTitle() + ";" +
+              getPublisher() + ";" +
+              getYear() + ";" +
+              month + ";" +
+              day + ";" +
+              language + "";
     }
 
     @Override
