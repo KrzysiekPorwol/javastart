cadef4402024f065de1cf70753e86dd8201f7235
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-08-18 07:51:52 +0200

    Start here

--- Project 2 - libary/Project 2 - libary/src/pl/javastart/library/exception/NoSuchFileTypeException.java
+++
@@ -1,7 +0,0 @@
-package pl.javastart.library.exception;
-
-public class NoSuchFileTypeException extends RuntimeException {
-    public NoSuchFileTypeException(String msg) {
-        super(msg);
-    }
-}
