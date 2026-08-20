b0932943b6cd1ab5334e4c19a8810f497f272c80
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-08-20 17:16:27 +0200

    Interfejsy Comparable i Comparator - cwiczenie 1

--- Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/App.java
+++ Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/App.java
@@ -1,4 +1,38 @@
 package pl.javastart.library.app;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class App {
+
+  static void main() {
+    Integer[] liczby = {5, 2, 8, 1, 9, 3, 7, 4, 6, 0,
+          15, 12, 18, 11, 19, 13, 17, 14, 16, 10};
+
+    System.out.println(Arrays.toString(liczby));
+
+    Arrays.sort(liczby, new Comparator<Integer>() {
+      @Override
+      public int compare(Integer o1, Integer o2) {
+        return o1 - o2;
+      }
+    });
+
+    System.out.println(Arrays.toString(liczby));
+
+    Arrays.sort(liczby, new Comparator<Integer>() {
+      @Override
+      public int compare(Integer o1, Integer o2) {
+        return o2 - o1;
+      }
+    });
+
+    System.out.println(Arrays.toString(liczby));
+
+
+  }
+
+
 }
+
+
---
+++ Project 2 - libary/Project 2 - libary/src/pl/javastart/library/model/Product.java
@@ -0,0 +1,48 @@
+package pl.javastart.library.model;
+
+class Product implements Comparable <Product> {
+
+  private String producer;
+  private String name;
+  private double price;
+
+  public Product(String producer, String name, double price) {
+    this.producer = producer;
+    this.name = name;
+    this.price = price;
+  }
+
+  public String getProducer() {
+    return producer;
+  }
+
+  public void setProducer(String producer) {
+    this.producer = producer;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  @Override
+  public String toString() {
+    return "Product [name=" + name + ", producer=" + producer + ", price=" + price + "]";
+  }
+
+  @Override
+  public int compareTo(Product o) {
+    return 0;
+  }
+}
