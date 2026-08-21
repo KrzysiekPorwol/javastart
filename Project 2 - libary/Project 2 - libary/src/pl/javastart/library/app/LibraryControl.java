a5526dc79d3d8ee8ffc6668e96e963932a4a4989
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-08-21 10:40:15 +0200

    Library v2.0

--- Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/LibraryControl.java
+++ Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/LibraryControl.java
@@ -12,7 +12,9 @@ import pl.javastart.library.model.Book;
 import pl.javastart.library.model.Library;
 import pl.javastart.library.model.Magazine;
 import pl.javastart.library.model.Publication;
+import pl.javastart.library.model.comparator.AlphabeticalTitleComparator;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
 
 class LibraryControl {
@@ -103,11 +105,6 @@ class LibraryControl {
     }
   }
 
-  private void printBooks() {
-    Publication[] publications = library.getPublications();
-    printer.printBooks(publications);
-  }
-
   private void addMagazine() {
     try {
       Magazine magazine = dataReader.readAndCreateMagazine();
@@ -117,11 +114,6 @@ class LibraryControl {
     } catch (ArrayIndexOutOfBoundsException e) {
       printer.printLine("Osiągnięto limit pojemności, nie można dodać kolejnego magazynu");
     }
-  }
-
-  private void printMagazines() {
-    Publication[] publications = library.getPublications();
-    printer.printMagazines(publications);
   }
 
   private void deleteMagazine() {
@@ -188,5 +180,21 @@ class LibraryControl {
         throw new NoSuchOptionException("Brak opcji o id " + option);
       }
     }
+  }
+
+  private void printBooks() {
+    Publication[] publications = getSortedPublications();
+    printer.printBooks(publications);
+  }
+
+  private void printMagazines() {
+    Publication[] publications = getSortedPublications();
+    printer.printMagazines(publications);
+  }
+
+  private Publication[] getSortedPublications() {
+    Publication[] publications = library.getPublications();
+    Arrays.sort(publications, new AlphabeticalTitleComparator());
+    return publications;
   }
 }
