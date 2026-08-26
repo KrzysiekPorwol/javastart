38dadabdd6026a45776d63487a992664d9158db7
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-08-26 15:52:53 +0200

    Wyrażenie lambda - cwiczenie 1

---
+++ Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/App2.java
@@ -0,0 +1,18 @@
+package pl.javastart.library.app;
+
+public class App2 {
+}
+
+
+
+
+//Zadanie
+//
+//      Napisz aplikację, która:
+//
+//Za pomocą klasy Random i interfejsu Supplier wygeneruje 12 losowych liczb z zakresu 1–50 i doda je do listy.
+//Wyświetli listę na ekranie za pomocą interfejsu Consumer, ale tym razem liczby mają być oddzielone przecinkiem i spacją,
+//np. 7, 23, 41, 5, ...
+//Napisze własną metodę, która przyjmuje listę oraz interfejs Predicate i za pomocą iteratora usunie z listy wszystkie
+//liczby większe od 25.
+//Ponownie wyświetli listę (znów przez Consumer), żeby pokazać wynik po usunięciu.
