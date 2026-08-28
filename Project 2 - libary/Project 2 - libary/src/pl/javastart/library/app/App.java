1e51e7ccdaaf63d263a34b9233bfc3569a079c96
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-08-28 18:17:12 +0200

    Referencja - cwiczenie 1

--- Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/App.java
+++ Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/App.java
@@ -1,4 +1,35 @@
 package pl.javastart.library.app;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class App {
+
+
+  static void main() {
+
+    List<String> names = new ArrayList<>();
+
+    names.add("Nikodem-ik");
+    names.add("Mareczkun-ik");
+    names.add("mikołaj-ik");
+    names.add("Krzysiul-ik");
+    names.add("Szymon-ik");
+
+
+
+
+    Collections.sort(names, String::compareToIgnoreCase);
+
+    for (String name : names) {
+      System.out.println(name);
+
+    }
+
+
+  }
+
+
 }
