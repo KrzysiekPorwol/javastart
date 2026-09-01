b371fbadaca8a7faf429fcf7bb4c0037fe1d973d
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-09-01 13:13:47 +0200

    Data i czas - cwiczenie 2 - poprawka

--- Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/App.java
+++ Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/App.java
@@ -2,24 +2,54 @@ package pl.javastart.library.app;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Scanner;
 
 public class App {
 
   static void main() {
     Scanner input = new Scanner(System.in);
-    System.out.println("W celu rozpoczecia odliczania czasu nalezy wcisnąc enter");
+    System.out.println("Podaj dzien miesiaca: ");
+    int day = input.nextInt();
     input.nextLine();
-    Instant actualTime1 = Instant.now();
-    System.out.println("W celu zakonczenia odliczania czasu nalezy wcisnąc enter");
+    System.out.println("Podaj miesiac: ");
+    int month = input.nextInt();
     input.nextLine();
-    Instant actualTime2 = Instant.now();
+    System.out.println("Podaj rok: ");
+    int year = input.nextInt();
+    input.nextLine();
 
-    Duration duration = Duration.between(actualTime1, actualTime2);
+    System.out.println("Podaj dzien miesiaca: ");
+    int day2 = input.nextInt();
+    input.nextLine();
+    System.out.println("Podaj miesiac: ");
+    int month2 = input.nextInt();
+    input.nextLine();
+    System.out.println("Podaj rok: ");
+    int year2 = input.nextInt();
+    input.nextLine();
 
-    System.out.println(duration.getSeconds());
+    LocalDate firstDate = LocalDate.of(year, month, day);
+    LocalDate secondDate = LocalDate.of(year2, month2, day2);
+
+    Period differenceTime;
+
+    if (firstDate.isAfter(secondDate)) {
+      System.out.println(firstDate + " jest później niż " + secondDate);
+      differenceTime = Period.between(secondDate, firstDate);   // od wcześniejszej do późniejszej
+    } else {
+      System.out.println(secondDate + " jest później niż " + firstDate);
+      differenceTime = Period.between(firstDate, secondDate);   // od wcześniejszej do późniejszej
+    }
+
+    System.out.println("Roznica wynosi " + differenceTime.getYears() + " lat, "
+          + differenceTime.getMonths() + " miesięcy i "
+          + differenceTime.getDays() + " dni");
 
 
+//    wyświetl informację o tym, która data jest "późniejsza",
+//    wyświetl liczbę lat, miesięcy i dni pomiędzy wprowadzonymi datami.
 
   }
 
