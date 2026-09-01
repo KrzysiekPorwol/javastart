c48dab2273a73c7d03ee57fa4f37253113a39137
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-09-01 12:53:04 +0200

    Data i czas - cwiczenie 1

--- Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/App.java
+++ Project 2 - libary/Project 2 - libary/src/pl/javastart/library/app/App.java
@@ -1,4 +1,26 @@
 package pl.javastart.library.app;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Scanner;
+
 public class App {
+
+  static void main() {
+    Scanner input = new Scanner(System.in);
+    System.out.println("W celu rozpoczecia odliczania czasu nalezy wcisnąc enter");
+    input.nextLine();
+    Instant actualTime1 = Instant.now();
+    System.out.println("W celu zakonczenia odliczania czasu nalezy wcisnąc enter");
+    input.nextLine();
+    Instant actualTime2 = Instant.now();
+
+    Duration duration = Duration.between(actualTime1, actualTime2);
+
+    System.out.println(duration.getSeconds());
+
+
+
+  }
+
 }
